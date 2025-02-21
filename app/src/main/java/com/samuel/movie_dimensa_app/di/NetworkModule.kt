1743bb6848fed74d485954dedcd4f410a55d515a
Samuu <samuelfilipevianadois@gmail.com>
2025-02-21 14:57:23 -0300

    feat: add network providers

--- app/src/main/java/com/samuel/movie_dimensa_app/di/NetworkModule.kt
+++ app/src/main/java/com/samuel/movie_dimensa_app/di/NetworkModule.kt
@@ -1,6 +1,15 @@
 package com.samuel.movie_dimensa_app.di
 
 import com.samuel.movie_dimensa_app.data.remote.APIKeyInterceptor
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMovieDetailsService
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMovieNowPlayingService
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMoviePopularService
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMovieReviewService
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMovieSimilarService
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMovieTopRatedService
+import com.samuel.movie_dimensa_app.data.remote.api.ApiMovieUpcomingService
+import com.samuel.movie_dimensa_app.data.remote.model.ApiMovieNowPlayingResponse
+import com.samuel.movie_dimensa_app.data.remote.model.ApiMovieReviewsResponse
 import com.samuel.movie_dimensa_app.utils.API_URL
 import dagger.Module
 import dagger.Provides
@@ -28,6 +37,41 @@ object NetworkModule {
       .client(okHttpClient)
       .addConverterFactory(GsonConverterFactory.create())
       .build()
+  }
+
+  @Provides
+  fun provideApiMovieDetailsService(retrofit: Retrofit): ApiMovieDetailsService {
+    return retrofit.create(ApiMovieDetailsService::class.java)
+  }
+
+  @Provides
+  fun provideApiMovieNowPlayingService(retrofit: Retrofit): ApiMovieNowPlayingService {
+    return retrofit.create(ApiMovieNowPlayingService::class.java)
+  }
+
+  @Provides
+  fun provideApiMoviePopularService(retrofit: Retrofit): ApiMoviePopularService {
+    return retrofit.create(ApiMoviePopularService::class.java)
+  }
+
+  @Provides
+  fun provideApiMovieReviewService(retrofit: Retrofit): ApiMovieReviewService {
+    return retrofit.create(ApiMovieReviewService::class.java)
+  }
+
+  @Provides
+  fun provideApiMovieSimilarService(retrofit: Retrofit) : ApiMovieSimilarService {
+    return retrofit.create(ApiMovieSimilarService::class.java)
+  }
+
+  @Provides
+  fun provideApiMovieTopRatedService(retrofit: Retrofit): ApiMovieTopRatedService {
+    return retrofit.create(ApiMovieTopRatedService::class.java)
+  }
+
+  @Provides
+  fun provideApiMovieUpcomingService(retrofit: Retrofit): ApiMovieUpcomingService {
+    return retrofit.create(ApiMovieUpcomingService::class.java)
   }
 
 }
