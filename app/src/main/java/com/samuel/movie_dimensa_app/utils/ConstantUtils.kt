be4b1054ca5944337dedcbe89e1abb08176126d2
Samuu <samuelfilipevianadois@gmail.com>
2025-02-24 17:02:41 -0300

    feat: add url utils

--- app/src/main/java/com/samuel/movie_dimensa_app/utils/ConstantUtils.kt
+++ app/src/main/java/com/samuel/movie_dimensa_app/utils/ConstantUtils.kt
@@ -3,5 +3,5 @@ package com.samuel.movie_dimensa_app.utils
 const val API_URL = "https://api.themoviedb.org/3/"
 const val ENGLISH = "en-US"
 const val PORTUGUESE = "pt-BR"
-const val POSTER_URL = "https://image.tmdb.org/t/p/w500/"
+const val POSTER_URL = "https://image.tmdb.org/t/p/w200/"
 const val BACKDROP_URL = "https://image.tmdb.org/t/p/original/"
